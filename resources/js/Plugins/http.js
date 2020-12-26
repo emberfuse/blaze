a4840a9ebdf22f8553f9c06e6947af804835ce36
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-12-26 19:05:17 +0530

    Update http module configurations

--- resources/js/Plugins/http.js
+++ resources/js/Plugins/http.js
@@ -1,8 +1,32 @@
 import Vue from "vue";
 import axios from "axios";
+import store from "@/Store";
+import router from "@/Router";
 
-window.axios = axios;
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-window.axios.defaults.withCredentials = true;
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+axios.defaults.withCredentials = true;
 
-Vue.prototype.$http = window.axios;
+axios.interceptors.request.use(request => {
+    const token = store.getters.token;
+
+    if (token) {
+        request.headers.common.Authorization = `Bearer ${token}`;
+    }
+
+    return request;
+});
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        const { status } = error.response;
+
+        if (status === 401 && store.getters.check) {
+            store.commit("logout");
+
+            router.push({ name: "login" });
+        }
+    }
+);
+
+Vue.prototype.$http = axios;
