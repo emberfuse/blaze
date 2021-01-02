9cbee03e615d413329669f480a3548f30c9bc5f6
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-02 15:19:05 +0530

    Update default user credentials

--- config/defaults.php
+++ config/defaults.php
@@ -8,14 +8,10 @@ return [
             'name' => 'Thavarshan Thayananthajothy',
             'username' => 'Thavarshan',
             'email' => 'tjthavarshan@gmail.com',
-            'phone' => '0775018795',
             'password' => '$2y$04$uitMi8kxfAHXz.aAWujG/.SeRuXwkVMuLJP/xLpLeWgyhZ5OUDzf6', // alphaxion77
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
-            'settings' => [
-                'notifications_mobile' => 'everything',
-                'notifications_email' => [],
-            ],
+            'settings' => [],
         ],
 
         'permissions' => [
