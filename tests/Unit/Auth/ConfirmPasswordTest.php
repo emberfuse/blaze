fee747d710835453f9e88838897c2275db6158a0
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-10 20:13:25 +0530

    Add confirm password tests - incomplete

---
+++ tests/Unit/Auth/ConfirmPasswordTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Auth;
+
+use PHPUnit\Framework\TestCase;
+
+class ConfirmPasswordTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
