251f1ec48c222f2707d7675995507af5c8e50b0a
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-10 19:33:59 +0530

    Add network and class reflection helpers

---
+++ tests/Concerns/InteractsWithNetwork.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Concerns;
+
+trait InteractsWithNetwork
+{
+    /**
+     * Determine if an Internet connection is available.
+     *
+     * @param int $port
+     *
+     * @return bool
+     */
+    protected function isConnected(int $port = 80): bool
+    {
+        if (@fsockopen('http://example.com/', $port)) {
+            @fclose();
+
+            return true;
+        }
+
+        return false;
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,12 +3,16 @@
 namespace Tests;
 
 use Tests\Concerns\CreatesNewUser;
+use Tests\Concerns\InteractsWithNetwork;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Concerns\InteractsWithProtectedQualities;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use InteractsWithProtectedQualities;
+    use InteractsWithNetwork;
     use CreatesNewUser;
     use WithFaker;
 }
