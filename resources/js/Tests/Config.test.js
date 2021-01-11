f8e86d6b8462a8cff56a52480581e335310b6d5a
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-11 16:38:05 +0530

    Update config js test

--- resources/js/Tests/Config.test.js
+++ resources/js/Tests/Config.test.js
@@ -2,25 +2,25 @@ import Repository from '@/Config/Repository';
 
 describe('Configurations Repository Tests', () => {
     test('it can be instantiated', () => {
-        let config = new Repository({});
+        const config = new Repository({});
 
         expect(config).toBeInstanceOf(Repository);
     });
 
     test('it can get all config items from repository', () => {
-        let config = new Repository({ foo: 'bar' });
+        const config = new Repository({ foo: 'bar' });
 
         expect(config.all()).toEqual({ foo: 'bar' });
     });
 
     test('it can get value of specific key', () => {
-        let config = new Repository({ foo: 'bar' });
+        const config = new Repository({ foo: 'bar' });
 
         expect(config.get('foo')).toEqual('bar');
     });
 
     test('it will return default value if given specific key was not found', () => {
-        let config = new Repository({ foo: 'bar' });
+        const config = new Repository({ foo: 'bar' });
 
         expect(config.get('fuz', 'baz')).toEqual('baz');
     });
