32d0058830afded7c6ca513bb864f2771bbc5e13
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-27 00:22:07 +0530

    Update for 'Undefined property: Cratespace\Preflight\Installer\ComposerPackages::' error

--- src/Installer/ComposerPackages.php
+++ src/Installer/ComposerPackages.php
@@ -2,10 +2,12 @@
 
 namespace Cratespace\Preflight\Installer;
 
-use Symfony\Component\Process\Process;
+use Cratespace\Preflight\Console\Concerns\InteractsWithTerminal;
 
 class ComposerPackages extends Packages
 {
+    use InteractsWithTerminal;
+
     /**
      * List of preflight specific composer packages.
      *
@@ -37,10 +39,6 @@ class ComposerPackages extends Packages
             is_array($packages) ? $packages : $this->packages
         );
 
-        (new Process($command, base_path(), ['COMPOSER_MEMORY_LIMIT' => '-1']))
-            ->setTimeout(null)
-            ->run(function ($type, $output) {
-                $this->output->write($output);
-            });
+        $this->runProcess($command, base_path(), ['COMPOSER_MEMORY_LIMIT' => '-1']);
     }
 }
