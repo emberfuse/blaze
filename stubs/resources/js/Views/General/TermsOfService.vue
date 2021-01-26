75a3182dba02b3b9fcaf3555f6afa094ae12ff0e
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-27 00:42:05 +0530

    Add plcaeholders for views

--- stubs/resources/js/Views/General/TermsOfService.vue
+++ stubs/resources/js/Views/General/TermsOfService.vue
@@ -3,7 +3,7 @@
         <div class="pt-4 bg-gray-100">
             <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
                 <div>
-                    <jet-authentication-card-logo />
+                    <!-- Logo -->
                 </div>
 
                 <div v-html="terms" class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
@@ -14,13 +14,7 @@
 </template>
 
 <script>
-import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo'
-
 export default {
     props: ['terms'],
-
-    components: {
-        JetAuthenticationCardLogo,
-    },
 }
 </script>
