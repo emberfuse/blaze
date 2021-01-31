e486b20783731d8a23ba5cf03e37b1355eeedfed
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-01-31 22:45:17 +0530

    Remove two-factor authentication enabled attribute from inertia middleware

--- src/Http/Middleware/ShareInertiaData.php
+++ src/Http/Middleware/ShareInertiaData.php
@@ -29,9 +29,7 @@ class ShareInertiaData extends Middleware
                     return;
                 }
 
-                return array_merge($request->user()->toArray(), [
-                    'two_factor_enabled' => ! is_null($request->user()->two_factor_secret),
-                ]);
+                $request->user()->toArray();
             },
 
             'errorBags' => function () {
