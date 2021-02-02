055a7cff88a2982ff6a73ac02acc4f6820019d42
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-02 22:29:23 +0530

    Remove unnecessary concerns

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -3,11 +3,9 @@
 namespace Cratespace\Preflight\Http\Controllers;
 
 use Cratespace\Preflight\Http\Controllers\Concerns\ReturnsResponse;
-use Cratespace\Citadel\Http\Controllers\Concerns\InteractsWithContainer;
 use Cratespace\Citadel\Http\Controllers\Controller as CitadelController;
 
 class Controller extends CitadelController
 {
     use ReturnsResponse;
-    use InteractsWithContainer;
 }
