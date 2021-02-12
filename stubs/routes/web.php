2b9096305e1f87185dcf306d9824ef01d7e6b892
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-12 22:23:03 +0530

    Fix Auth guard [sanctum] is not defined error

--- stubs/routes/web.php
+++ stubs/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => Inertia::render('Marketing/Welcome'))->name('welcome');
 
-Route::middleware(['auth:sanctum', 'verified'])->get(
+Route::middleware(['auth:sentinel', 'verified'])->get(
     '/home',
     fn () => Inertia::render('Business/Home')
 )->name('home');
