6717115772099230317442450799d8ea99e7652a
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-19 22:50:33 +0530

    Add new generator commands

---
+++ src/Support/InteractsWithModel.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+trait InteractsWithModel
+{
+    /**
+     * Instance of model being queried.
+     *
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    protected static $model;
+
+    /**
+     * Instantiate the model to be queried.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    protected static function model(): Model
+    {
+        return app()->make(static::$model);
+    }
+}
---
+++ tests/FilterMakeCommandTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Cratespace\Preflight\Tests;
+
+class FilterMakeCommandTest extends TestCase
+{
+    /**
+     * Name of test filter class.
+     *
+     * @var string
+     */
+    protected $testFilter;
+
+    /**
+     * Name of filter class.
+     *
+     * @var string
+     */
+    protected $name = 'MockFilter';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->testFilter = app_path("Filters/{$this->name}.php");
+
+        $this->deleteFile($this->testFilter);
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->deleteFile($this->testFilter);
+    }
+
+    public function testCreateFilterClass()
+    {
+        $this->artisan('make:filter', ['name' => $this->name])->assertExitCode(0);
+
+        $this->assertTrue(is_file($this->testFilter));
+    }
+}
