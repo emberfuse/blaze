fc8139083735a79bdd0f7cdd90ddd26ebd9056fd
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-03-19 16:35:10 +0530

    Fix foreign key violation error on permission_role table

--- database/migrations/2021_03_19_110157_create_permission_role_table.php
+++ database/migrations/2021_03_19_110157_create_permission_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePermissionRole extends Migration
+class CreatePermissionRoleTable extends Migration
 {
     /**
      * Run the migrations.
