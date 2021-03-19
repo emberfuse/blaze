fc25bffb4f17fcbc3e7e7667572c18bef6a146d6
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-03-19 12:29:38 +0530

    Add path attribute trait

---
+++ src/Models/Traits/Directable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Cratespace\Preflight\Models\Traits;
+
+use Illuminate\Support\Str;
+
+trait Directable
+{
+    /**
+     * Get full path to single resource page.
+     *
+     * @return string
+     */
+    public function getPathAttribute(): string
+    {
+        $index = property_exists($this, 'index')
+            ? $this->index
+            : $this->getIndex();
+
+        return route("{$index}.show", $this);
+    }
+
+    /**
+     * Get resource index.
+     *
+     * @return string
+     */
+    protected function getIndex(): string
+    {
+        if (property_exists($this, 'index')) {
+            return $this->index;
+        }
+
+        return Str::plural(strtolower(class_basename($this)));
+    }
+}
