8ae011d277f8d209691ba1ed9afcbc9e0af07f85
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-05-06 13:01:17 +0530

    Make query and presenter classes into independant modules, refactor query class

--- src/Queries/Query.php
+++ src/Queries/Query.php
@@ -2,8 +2,36 @@
 
 namespace Cratespace\Preflight\Queries;
 
-use Cratespace\Preflight\Support\AbstractEloquent;
+use RuntimeException;
+use Illuminate\Database\Eloquent\Builder;
+use Cratespace\Sentinel\Support\Concerns\InteractsWithContainer;
 
-class Query extends AbstractEloquent
+abstract class Query
 {
+    use InteractsWithContainer;
+
+    /**
+     * Instance of model being queried.
+     *
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    protected $model;
+
+    /**
+     * Begin querying the model.
+     *
+     * @return Builder
+     */
+    public function query(): Builder
+    {
+        if (is_null($this->model)) {
+            throw new RuntimeException('Model not set');
+        }
+
+        if (is_string($this->model)) {
+            $this->model = $this->resolve($this->model);
+        }
+
+        return $this->model->query();
+    }
 }
